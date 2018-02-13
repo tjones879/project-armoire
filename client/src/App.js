9e11591df69b2fb248ef6e6d34e04fbc0ccfa0ee
Tyler Jones <tdjones879@gmail.com>
2018-02-13 12:47:07 -0700

    Implement stateful textarea for submissions.

--- client/src/App.js
+++ client/src/App.js
@@ -72,14 +72,44 @@ class TestResults extends Component {
   }
 }
 
+class SubmissionForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "Please enter your text here."
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A submission was entered: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <textarea value={this.state.value} onChange={this.handleChange} rows="25" cols="85"/>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 class Submission extends Component {
   render() {
     return (
       <div>
-        <h3>Text Area</h3>
-        <textarea rows="4" cols="85">
-        </textarea>
-        <TestResults tests={this.props.tests}/>
+        <SubmissionForm />
+        <div>
+          <TestResults tests={this.props.tests}/>
+        </div>
       </div>
     );
   }
