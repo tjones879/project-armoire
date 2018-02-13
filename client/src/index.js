5df9e59ba657c5378e5d96e81fad69c67bf21ddb
Tyler Jones <tdjones879@gmail.com>
2018-02-13 16:37:21 -0700

    Add react-router-dom for routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Assignment from './components/Assignment.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+    return (
+      <Router>
+        <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/assignment">Assignments</Link></li>
+        </ul>
+          <Route path="/assignment" component={Assignment} />
+        </div>
+      </Router>
+    );
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
