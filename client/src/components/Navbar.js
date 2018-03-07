1bbf812bdcb95015fd7951632dec57516d229006
Dalton Neely <djneely@mavs.coloradomesa.edu>
2018-03-07 15:27:51 -0700

    fixed json.lock issue added boiler plate navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+export class Navbar extends Component{
+    render(){
+        return(
+            <nav className="navbar navbar-expand-lg navbar-light">
+            </nav>
+        );
+    }
+}
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Navbar} from './Navbar'
 
 var registerAPI = "localhost:3000/";
 
