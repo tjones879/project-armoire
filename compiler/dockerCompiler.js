111454daa5bead8dfed463672c3f2281d7da2b60
Tyler Jones <tdjones879@gmail.com>
2018-03-27 18:21:25 -0600

    Rework how child processes are executed.

--- compiler/dockerCompiler.js
+++ compiler/dockerCompiler.js
@@ -18,7 +18,6 @@ DockerSandbox.prototype.run = function(success)
     var sandbox = this;
 
     this.prepare(() => {
-        console.log(sandbox);
         sandbox.execute(success);
     });
 }
@@ -35,14 +34,14 @@ DockerSandbox.prototype.prepare = function(success)
     exec(command, (st) => {
         fs.writeFile(sandbox.path + sandbox.folder + "/" + sandbox.file_name, sandbox.code, (err) => {
             if (err) {
-                console.log(err);
+                console.log("Error in writing code: " + err);
             } else {
                 console.log(sandbox.langName + " file was saved!");
                 exec("chmod 777 \'" + sandbox.path + sandbox.folder + "/" + sandbox.file_name + "\'");
 
                 fs.writeFile(sandbox.path + sandbox.folder + "/inputFile", sandbox.stdin_data, (err) => {
                     if (err) {
-                        console.log(err);
+                        console.log("Error in writing input: " + err);
                     } else {
                         console.log("Input file was saved!");
                         success();
@@ -53,6 +52,14 @@ DockerSandbox.prototype.prepare = function(success)
     });
 }
 
+function buildDockerCmd(path, vmName, compiler, srcFile, outputCommand, extraArgs) {
+    let command = 'sudo docker run --rm -v ';
+    command += '"' + path + '":/codeDir ';
+    command += vmName + ' /codeDir/script.sh ';
+    command += compiler + ' ' + srcFile + ' ' + outputCommand + ' ' + extraArgs;
+    return command;
+}
+
 DockerSandbox.prototype.execute = function(success)
 {
     var exec = require('child_process').exec;
@@ -60,68 +67,85 @@ DockerSandbox.prototype.execute = function(success)
     var timer = 0; //variable to enforce the timeout_value
     var sandbox = this;
 
-    var st = this.path + 'dockerRun.sh ' + this.timeout_value + 's -u mysql -e \'NODE_PATH=/usr/local/lib/node_modules\' -i -t -v  "' + this.path + this.folder + '":/codeDir ' + this.vm_name + ' /codeDir/script.sh ' + this.compiler_name + ' ' + this.file_name + ' ' + this.output_command+ ' ' + this.extra_arguments;
-    console.log(st);
+    var st = buildDockerCmd(this.path + this.folder, this.vm_name,
+                            this.compiler_name, this.file_name,
+                            this.output_command, this.extra_arguments);
+    console.log("Executing script: `" + st + "`");
 
-    exec(st);
-    console.log("*************************")
-    var intid = setInterval(() => {
-        timer = timer + 1;
+    let defaults = {
+        encoding: 'utf8',
+        timeout: this.timeout_value,
+        maxBuffer: 200 * 1024,
+        killSignal: 'SIGTERM',
+        cwd: null,
+        env: null
+    };
 
-        fs.readFile(sandbox.path + sandbox.folder + '/completed', 'utf8', (err, data) => {
-            if (err && timer < sandbox.timeout_value) {
-                return;
-            } else if (timer < sandbox.timeout_value) {
-                console.log("DONE")
-                fs.readFile(sandbox.path + sandbox.folder + '/errors', 'utf8', (err2, data2) => {
-                    if(!data2)
-                        data2 = "";
-
-                    console.log("Error file: ")
-                    console.log(data2)
-
-                    console.log("Main File")
-                    console.log(data)
-
-                    var lines = data.toString().split('COMPILER END')
-                    data=lines[0]
-                    var time=lines[1]
-
-                    console.log("Time: ")
-                    console.log(time)
-
-                    success(data,time,data2)
-                });
-            } else {
-                fs.readFile(sandbox.path + sandbox.folder + '/logfile.txt', 'utf8', (err, data) => {
-                    if (!data)
-                        data = "";
-
-                    data += "\nExecution Timed Out";
-                    console.log("Timed Out: " + sandbox.folder + " " + sandbox.langName);
+    let dockerPID = exec(st, defaults, (err, stdout, stdin) => {
+        if (err) {
+            console.log("Compilation timed out.");
+            let data = "\nExecution Timed Out";
+            success(data, defaults.timeout, "");
+        } else {
+            fs.readFile(sandbox.path + sandbox.folder + '/completed', 'utf8', (err, data) => {
+                if (!err) {
+                    // The compiler must have finished if the file exists.
+                    console.log("DONE");
                     fs.readFile(sandbox.path + sandbox.folder + '/errors', 'utf8', (err2, data2) => {
                         if(!data2)
                             data2 = "";
 
-                        var lines = data.toString().split('*---*');
-                        data = lines[0];
-                        var time = lines[1];
+                        console.log("Error file: ")
+                        console.log(data2)
 
-                        console.log("Time: ");
-                        console.log(time);
+                        console.log("Main File")
+                        console.log(data)
 
-                        success(data,data2)
+                        var lines = data.toString().split('COMPILER END')
+                        data=lines[0]
+                        var time=lines[1]
+
+                        console.log("Time: ")
+                        console.log(time)
+
+                        success(data,time,data2)
                     });
+                }
+            });
+            /*
+               } else {
+                console.log("completed: " + data);
+                fs.readFile(sandbox.path + sandbox.folder + '/logfile.txt', 'utf8', (err, data) => {
+                    if (err) {
+                        console.log("Compiler has thrown a sys error: " + err);
+                    } else if (!data) {
+                        data = "";
+                    } else {
+                        data += "\nExecution Timed Out";
+                        console.log("Timed Out: " + sandbox.folder + " " + sandbox.langName);
+                        fs.readFile(sandbox.path + sandbox.folder + '/errors', 'utf8', (err2, data2) => {
+                            if(!data2)
+                                data2 = "";
+
+                            var lines = data.toString().split('*---*');
+                            data = lines[0];
+                            var time = lines[1];
+
+                            console.log("Time: ");
+                            console.log(time);
+
+                            success(data,data2)
+                        });
+                    }
                 });
             }
-
-            console.log("Removing temp folder: " + sandbox.folder);
-            console.log("*************************");
-            exec("rm -r " + sandbox.folder);
-
-            clearInterval(intid);
-        });
-    }, 1000);
+        }
+        */
+        }
+        console.log("Removing temp folder: " + sandbox.folder);
+        console.log("*************************");
+        exec("rm -r " + sandbox.folder);
+    });
 }
 
 module.exports = DockerSandbox;
--- compiler/index.js
+++ compiler/index.js
@@ -38,7 +38,7 @@ app.post('/compile', bruteforce.prevent, (req, res) =>
     let code = req.body.code;
 
     let payload = {
-        timeout_value:   5, //seconds
+        timeout_value:   1000, // ms
         path:            __dirname + "/",
         folder:          'temp/' + random(10),
         vm_name:         'virtual_machine',
