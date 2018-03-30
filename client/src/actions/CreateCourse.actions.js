f803c066918c49bef0cb278b796445a47e63bde3
Dalton Neely <djneely@mavs.coloradomesa.edu>
2018-03-30 12:57:46 -0600

    test

---
+++ client/src/actions/CreateCourse.actions.js
@@ -0,0 +1,3 @@
+import dispatcher from '../dispatcher';
+
+export function changeInput()
--- client/src/containers/CreateCourse.container.react.js
+++ client/src/containers/CreateCourse.container.react.js
@@ -2,15 +2,48 @@ import React from 'react';
 import {Component} from 'react';
 import {PropTypes} from 'prop-types';
 
+import {Input} from '../components/input.component.react';
+import {Btn} from '../components/Btn.component.react';
+
 export class CreateCourseContainer extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            form:{
+                elements:[
+                    {
+                        type:'text',
+                        id:'cName',
+                        name:'courseName',
+                        text:'Course Name'
+                    },
+                    {
+                        type:'text',
+                        id:'cNum',
+                        name:'courseNumber',
+                        text:'Course Number'
+                    }
+                ]
+            }
+        }
+
+        this.validate = this.validate.bind(this);
     }
+
+    validate(event){
+        
+        event.preventDefault();
+    }
+
     render(){
         return(
-            <form>
+            <form onSubmit={this.validate}>
                 <fieldset>
                     <legend>{this.props.title}</legend>
+                    {this.state.form.elements.map(element =>
+                        <Input key={element.id} type={element.type} id={element.id} name={element.name} text={element.text} />
+                    )}
+                    <Btn type='submit' text='Create Course'/>
                 </fieldset>
             </form>
         );
