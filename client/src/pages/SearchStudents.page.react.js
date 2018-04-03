76dc75fd310eaa63dd415e95a71a5ef73f6682ec
djneely <djneely@mavs.coloradomesa.edu>
2018-04-03 15:03:46 -0600

    added user redirection on search students page

--- client/src/pages/SearchStudents.page.react.js
+++ client/src/pages/SearchStudents.page.react.js
@@ -2,7 +2,18 @@ import React, {Component} from 'react';
 
 import {Navbar} from '../components/Navbar';
 
+import AuthService from '../components/AuthService';
+
 export class SearchStudentsPage extends Component{
+    constructor(){
+        super();
+        this.Auth = new AuthService();
+    }
+    componentWillMount(){
+        if(!this.Auth.loggedIn()){
+            window.location = "login";
+        }
+    }
     render(){
         return(
             <div>
