1b5d0052bebcc446d0b2c4a9cc91b57df558bca5
djneely <djneely@mavs.coloradomesa.edu>
2018-04-03 14:46:35 -0600

    added link to student API

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -19,7 +19,8 @@ class Home extends Component {
                 <h1>Welcome to Project Armoire</h1>
                 <a href="https://github.com/tjones879/project-armoire" target="_blank" rel="noopener noreferrer">GitHub Link</a><br/>
                 <a href="https://trello.com/b/aYuzLHk3/project-armoire" target="_blank" rel="noopener noreferrer">Trello Board</a><br />
-                <a href="https://github.com/tjones879/project-armoire/wiki/API" target="_blank" rel="noopener noreferrer">API</a>
+                <a href="https://github.com/tjones879/project-armoire/wiki/API" target="_blank" rel="noopener noreferrer">API</a><br />
+                <a href="https://github.com/tjones879/project-armoire/wiki/API---Student" target="_blank" rel="noopener noreferrer">Student Endpoint API</a>
             </div>
         );
     }
