9837eb1446e538d10544eee250a9788258202831
Sayre Thomas <sayrethomasis@gmail.com>
2018-04-09 09:03:03 -0600

    Add Prof Page in API

--- backend/routes/professor.js
+++ backend/routes/professor.js
@@ -8,16 +8,49 @@ var router = express.Router();
 let Professor = require('../db/professor.js');
 var Mongoose = require('mongoose');
 
-/* GET professors listing. */
-router.get('/', function(req, res, next) {
-    res.json({
-        id: "ObjectId",
-        fname: "John",
-        lname: "Smith",
-        courses: ["CS 101", "Software Engineering", "User Interface Design"]
-    });
+/* GET  ALL professors listing. */
+router.get('/', (req, res) => {
+    Professor.find().then((arr) => {
+        if(arr != null){
+            res.send(arr);
+        }
+        else{
+            res.send([]);
+        }
+    }).catch((err) => {console.log(err.message);res.send([]);})
 });
 
+/* GET login_id/id professor listing. */
+router.get('/login_id/:id', (req, res) => {
+    console.log(req.params.id);
+    let id = new Mongoose.Types.ObjectId(req.params.id);
+    Professor.find({login_id:id}).then((obj) => {
+        if(obj != null){
+            res.json(obj);
+        }
+        else{
+            res.json({});
+        }
+    }).catch((err) => {console.log(err.message);res.json({});})
+})
+
+/* GET /id professor listing. */
+router.get('/:id', (req, res) => {
+    console.log(req.params.id);
+    Professor.findById(req.params.id).then((obj) => {
+        if(obj != null){
+            res.json(obj);
+        }
+        else{
+            res.json([]);
+        }
+    }).catch((err) => {
+        console.log(err.message);
+        res.json({});
+    })
+})
+
+/* POST a new professo listing. */
 router.post('/', (req, res, next) => {
     if (req.body.id !== undefined && req.body.email !== undefined){
         Professor.find({login_id: new Mongoose.Types.ObjectId(req.body.id)}, (err, obj) => {
